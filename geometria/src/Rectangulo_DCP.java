76fcd1ad8625d330b237466cdb9479abb6dd0bf3
Diego Candela <diego@severo.com>
2019-02-01 19:02:20 +0100

    Versión refactorizada

--- geometria/src/Rectangulo_DCP.java
+++ geometria/src/Rectangulo_DCP.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_DCP extends FiguraGeometrica_DCP {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_DCP(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
